9eb566df4f3e5a97cdbb8968691726d4adaa70b4
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2021-05-08 03:56:37 +0500

    mini fix

--- src/main/java/ru/netology/manager/FilmManager.java
+++ src/main/java/ru/netology/manager/FilmManager.java
@@ -6,40 +6,40 @@ import ru.netology.repository.FilmRepository;
 
 @NoArgsConstructor
 public class FilmManager {
-  private FilmRepository repository;
+    private FilmRepository repository;
 
-  public FilmManager(FilmRepository repository) {
-    this.repository = repository;
-  }
-
-  public void add(FilmPoster films){
-    repository.save(films);
-  }
-
-  public FilmPoster[] getAll(){
-    int maxShowFilms = 10;
-    FilmPoster[] films = repository.findAll();
-    FilmPoster[] result;
-
-    if (films.length < maxShowFilms) {
-      result = new FilmPoster[films.length];
-    } else {
-      result = new FilmPoster[maxShowFilms];
+    public FilmManager(FilmRepository repository) {
+        this.repository = repository;
     }
 
-    for (int i = 0; i < result.length; i++) {
-      int index = films.length -i - 1;
-      result[i] = films[index];
+    public void add(FilmPoster films) {
+        repository.save(films);
     }
 
-    return result;
-  }
+    public FilmPoster[] getAll() {
+        int maxShowFilms = 10;
+        FilmPoster[] films = repository.findAll();
+        FilmPoster[] result;
 
-  public void removeById(int id){
-    repository.removeById(id);
-  }
+        if (films.length < maxShowFilms) {
+            result = new FilmPoster[films.length];
+        } else {
+            result = new FilmPoster[maxShowFilms];
+        }
 
-  public FilmPoster[] removeAll(){
-    return repository.removeAll();
-  }
+        for (int i = 0; i < result.length; i++) {
+            int index = films.length - i - 1;
+            result[i] = films[index];
+        }
+
+        return result;
+    }
+
+    public void removeById(int id) {
+        repository.removeById(id);
+    }
+
+    public FilmPoster[] removeAll() {
+        return repository.removeAll();
+    }
 }
