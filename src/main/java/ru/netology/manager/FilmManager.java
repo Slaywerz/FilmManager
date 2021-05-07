40fde790cabceccbaaa23e889f7f081d0b99a363
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2021-05-08 03:54:49 +0500

    getAddedFilms changed

--- src/main/java/ru/netology/manager/FilmManager.java
+++ src/main/java/ru/netology/manager/FilmManager.java
@@ -5,29 +5,31 @@ import ru.netology.domain.FilmPoster;
 
 @NoArgsConstructor
 public class FilmManager {
-  private FilmPoster[] films = new FilmPoster[0];
-  int maxShowFilm = 10;
+    private FilmPoster[] films = new FilmPoster[0];
+    int maxShowFilm = 10;
 
 
-
-  public void add(FilmPoster film) {
-    int length = films.length + 1;
-    FilmPoster[] tmp = new FilmPoster[length];
-    System.arraycopy(films, 0, tmp, 0, films.length);
-    int lastIndex = tmp.length - 1;
-    tmp[lastIndex] = film;
-    films = tmp;
-  }
-
-  public FilmPoster[] getAddedFilms() {
-    FilmPoster[] result = new FilmPoster[films.length];
-    if (films.length > maxShowFilm) {
-      result = new FilmPoster[maxShowFilm];
+    public void add(FilmPoster film) {
+        int length = films.length + 1;
+        FilmPoster[] tmp = new FilmPoster[length];
+        System.arraycopy(films, 0, tmp, 0, films.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = film;
+        films = tmp;
     }
-    for (int i = 0; i < result.length; i++) {
-      int index = films.length - i - 1;
-      result[i] = films[index];
+
+    public FilmPoster[] getAddedFilms() {
+        FilmPoster[] result;
+        if (films.length < maxShowFilm) {
+            result = new FilmPoster[films.length];
+        } else {
+            result = new FilmPoster[maxShowFilm];
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            int index = films.length - i - 1;
+            result[i] = films[index];
+        }
+        return result;
     }
-    return result;
-  }
 }
